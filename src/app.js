b5e61e22759bafff0738f27918ed0409e3e1bc26
Jimmbo19 <jega190112@gmail.com>
2024-08-17 04:01:21 +0000

    carta simple

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function cartaaleatoria() {
+  const carta = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const mazo = [" ♦ ", "♥ ", "♠ ", "♣"];
+
+  let cartaazar = carta[Math.floor(Math.random() * carta.length)];
+  let mazoazar = mazo[Math.floor(Math.random() * mazo.length)];
+
+  document.getElementById("cartanumero").innerHTML = cartaazar;
+  document.getElementById("mazoclase").innerHTML = mazoazar;
+  document.getElementById("mazoclase2").innerHTML = mazoazar;
 };
